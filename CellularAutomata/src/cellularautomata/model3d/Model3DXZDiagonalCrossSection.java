fd527d7b8e205e530c3422821778b2c150ba0af7
Jaume Ribas <r.ribas.jaume@gmail.com>
2022-01-03 21:26:41 +0100

    Updated license year :D

--- CellularAutomata/src/cellularautomata/model3d/Model3DXZDiagonalCrossSection.java
+++ CellularAutomata/src/cellularautomata/model3d/Model3DXZDiagonalCrossSection.java
@@ -1,5 +1,5 @@
 /* Aether2DImgMaker -- console app to generate images of the Aether cellular automaton in 2D
-    Copyright (C) 2017-2021 Jaume Ribas
+    Copyright (C) 2017-2022 Jaume Ribas
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
