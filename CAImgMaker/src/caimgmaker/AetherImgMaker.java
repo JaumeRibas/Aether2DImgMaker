e394803d2b88061e2724eded5e557831b9c82578
Jaume Ribas <r.ribas.jaume@gmail.com>
2022-08-21 15:57:36 +0200

    v0.5.4

--- CAImgMaker/src/caimgmaker/AetherImgMaker.java
+++ CAImgMaker/src/caimgmaker/AetherImgMaker.java
@@ -103,7 +103,7 @@ public class AetherImgMaker {
 				return;
 			}
 			if (args.outputVersion) {
-				System.out.println("0.5.3");
+				System.out.println("0.5.4");
 				return;
 			}
 			Model model = getModel(args);
