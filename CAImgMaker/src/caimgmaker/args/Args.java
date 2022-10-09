0bc1f8a6f7897ef857d98899914b481cd17bc8d2
Jaume Ribas <r.ribas.jaume@gmail.com>
2022-10-09 20:24:49 +0200

    Fixes in -first-step option description

--- CAImgMaker/src/caimgmaker/args/Args.java
+++ CAImgMaker/src/caimgmaker/args/Args.java
@@ -68,7 +68,7 @@ public class Args {
 	@Parameter(names = { "-restore" }, description = "The path of the backup to restore. Mandatory when no initial configuration is passed and the selected -model requires one.")
     public String backupToRestorePath = null;
 	
-	@Parameter(names = { "-first-step" }, validateWith = NonNegativeIntegerValidator.class, description = "The app skips ahead to this step, or the model's last step in case it is smaller, without generating images.")
+	@Parameter(names = { "-first-step" }, validateWith = NonNegativeIntegerValidator.class, description = "The app skips ahead to this step or the model's last step plus one, in case it is smaller, without generating images.")
 	public long initialStep = 0;
 	
 	@Parameter(names = { "-step-leap" }, validateWith = GreaterThanZeroIntegerValidator.class, description = "The app will generate images at the steps multiple of this number. If this number is greater than one, the app will also generate an image at the last step plus one of the model.")
