5919dbc795c0e9f1bbebbe34f8a830c7689b2364
Jaume Ribas <r.ribas.jaume@gmail.com>
2023-04-09 20:24:40 +0200

    Added missing ModelAs1D and ModelAs2D subclasses

---
+++ CellularAutomata/src/cellularautomata/model1d/LongModelAs1D.java
@@ -0,0 +1,33 @@
+/* Aether2DImgMaker -- console app to generate images of the Aether cellular automaton in 2D
+    Copyright (C) 2017-2023 Jaume Ribas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package cellularautomata.model1d;
+
+import cellularautomata.Coordinates;
+import cellularautomata.model.LongModel;
+
+public class LongModelAs1D extends ModelAs1D<LongModel> implements LongModel1D {
+
+	public LongModelAs1D(LongModel source) {
+		super(source);
+	}
+
+	@Override
+	public long getFromPosition(int x) throws Exception {
+		return source.getFromPosition(new Coordinates(x));
+	}
+
+}
---
+++ CellularAutomata/src/cellularautomata/model1d/ObjectModelAs1D.java
@@ -0,0 +1,33 @@
+/* Aether2DImgMaker -- console app to generate images of the Aether cellular automaton in 2D
+    Copyright (C) 2017-2023 Jaume Ribas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package cellularautomata.model1d;
+
+import cellularautomata.Coordinates;
+import cellularautomata.model.ObjectModel;
+
+public class ObjectModelAs1D<Object_Type> extends ModelAs1D<ObjectModel<Object_Type>> implements ObjectModel1D<Object_Type> {
+
+	public ObjectModelAs1D(ObjectModel<Object_Type> source) {
+		super(source);
+	}
+
+	@Override
+	public Object_Type getFromPosition(int x) throws Exception {
+		return source.getFromPosition(new Coordinates(x));
+	}
+
+}
